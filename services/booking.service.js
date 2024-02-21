67b35307da2c7d4c556d2b290347c710d220bbb5
root <root@ybaycizgpo.local>
2024-02-21 14:17:57 +0000

    to prod

--- services/booking.service.js
+++ services/booking.service.js
@@ -25,7 +25,7 @@ class bookingService {
             return res
 
         } catch (error) {
-            console.error("error while adding booking")
+            // console.error("error while adding booking")
             pino.info(error)
             return error
         }
